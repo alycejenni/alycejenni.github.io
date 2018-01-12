c6374979788114af7970e070aeb3d1e7a019dd07
Alice Butcher <alycejenni@gmail.com>
2018-01-12 15:00:05 +0000

    :construction: reverting back to the old build script with a few modifications

--- build.sh
+++ build.sh
@@ -1,5 +1,30 @@
-#!/usr/bin/env bash
-set -e # halt script on error
+#! /bin/bash
 
+set -e
+
+DEPLOY_REPO="https://${GITHUB_TOKEN}@github.com/alycejenni/alycejenni.github.io.git"
+
+# enable error reporting to the console
+set -e
+
+# cleanup
+ls -A
+rm -rf alycejenni.github.io
+
+# clone dev
+git clone -b dev ${DEPLOY_REPO}
+cd alycejenni.github.io
+
+# build
 bundle exec jekyll build
-bundle exec htmlproofer ./_site
+
+# copy the git repo into the _site folder
+cp -r .git _site/
+cd _site
+
+# push
+git config user.email "alycejenni@gmail.com"
+git config user.name "Alice Butcher"
+git add --all
+git commit -a -m ":rocket: #$TRAVIS_BUILD_NUMBER"
+git push --force --no-verify origin/master
