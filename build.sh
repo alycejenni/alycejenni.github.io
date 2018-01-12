c00bbecf9f7112a097a725e81effe13ea7220242
Alice Butcher <alycejenni@gmail.com>
2018-01-12 11:57:02 +0000

    :green_heart: trying again

--- build.sh
+++ build.sh
@@ -1,23 +1,5 @@
-#! /bin/bash
-# shamelessly lifted from https://ayastreb.me/deploy-jekyll-to-github-pages-with-travis-ci/
+#!/usr/bin/env bash
+set -e # halt script on error
 
-set -e
-
-REPO="https://github.com/alycejenni/alycejenni.github.io.git"
-
-# skip if build is triggered by pull request
-if [ $TRAVIS_PULL_REQUEST == "true" ]; then
-  exit 0
-fi
-
-# enable error reporting to the console
-set -e
-
-rm -rf alycejenni.github.io
-
-# clone remote repo
-git clone -b dev ${REPO}
-cd alycejenni.github.io
-
-# build with Jekyll
 bundle exec jekyll build
+bundle exec htmlproofer ./_site
